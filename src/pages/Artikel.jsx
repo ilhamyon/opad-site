76f8f17bfd2296296917a214bac998a1567fedef
ilhamyon <ilhamyon@gmail.com>
2024-02-29 19:22:54 +0700

    fix: pdf view width

--- src/pages/Artikel.jsx
+++ src/pages/Artikel.jsx
@@ -1,11 +1,10 @@
 import { Document, Page, pdfjs } from "react-pdf"
-import SampelPdf from "../assets/sampel.pdf"
 
 // Set worker URL to enable worker support (replace this with your own workerSrc)
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 // Path to your PDF file
-const pdfUrl = 'sampel.pdf';
+const pdfUrl = '/sampel.pdf';
 
 function Artikel() {
   return (
@@ -14,7 +13,7 @@ function Artikel() {
             <h2 className="text-xl font-bold px-4 py-4">Artikel</h2>
         </div>
 
-        <div>
+        <div className="px-4 py-6 w-[480px]">
           {/* <embed
             src={SampelPdf}
             type="application/pdf"
@@ -26,7 +25,7 @@ function Artikel() {
           {/* <object data="/sampel.pdf" type="application/pdf" width="100%" height="600">
             <p>Maaf, file PDF tidak dapat dimuat. Silakan periksa kembali nanti.</p>
           </object> */}
-          <Document file={SampelPdf}>
+          <Document file={pdfUrl}>
             <Page pageNumber={1} />
           </Document>
         </div>
